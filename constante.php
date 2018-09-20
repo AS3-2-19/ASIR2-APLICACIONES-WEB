1cd52f8571be09b9906dac70ead5a63d7916147b
AS3-2-19 <AS3-2-19@uni.lan>
2018-09-20 13:44:36 +0200

    horario de clase

--- constante.php
+++ constante.php
@@ -1,10 +1,10 @@
 <?php
 $t = date("H");
 
-if ($t < "20") // si "$t" es menor que dos
+
+if ($t < "13") // si "$t" es menor que dos
 {
     echo "Have a good day!"; //si el if es true imprimira el echo
 }else {
     echo "have a bad day";
 }
-?>
---
+++ horario de clase.php
@@ -0,0 +1,13 @@
+<?php
+//$t = date("H");
+$t = 8
+
+if ($t =< "8") 
+{
+    echo "servicios de redes";
+}
+else {
+    echo "no tenemos redes"
+}
+
+?>
