ce8627af55d3138820f14182e377f31a2023abd1
AS3-2-19 <AS3-2-19@uni.lan>
2018-09-27 12:34:37 +0200

    foreach

--- array.php
+++ array.php
@@ -1,4 +1,25 @@
 <?php
-$cars = array("Volvo", "BMW", "Toyota");
-echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+//$cars = array("Volvo", "BMW", "Toyota");
+//echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+
+//for ($x = 0;$x< count($cars);$x++){
+    //echo $cars[$x];
+    //echo "<br>";
+
+//}
+
+
+
+
+$ekaitz = array("tonto", "simple", "feo");
+echo "ekitz es:";
+echo "<br>";
+//echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+
+for ($x = 0;$x< count($ekaitz);$x++){
+    echo $ekaitz[$x];
+    echo "<br>";
+
+}
+
 ?>
---
+++ foreach.php
@@ -0,0 +1,12 @@
+<?php
+
+$suma= array[
+    array[1,2,],
+    array[3,4]
+];
+
+echo "resultado".$suma[1][1]
+
+foreach ($suma as $value){
+    echo "$value <br>";
+}
