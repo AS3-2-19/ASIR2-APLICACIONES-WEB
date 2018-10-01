48804d59e820a6a1dc286a1213605454ce46f144
AS3-2-19 <AS3-2-19@uni.lan>
2018-10-01 12:42:31 +0200

    foreach

--- foreach.php
+++ foreach.php
@@ -1,12 +1,28 @@
-<?php
 
-$suma= array[
-    array[1,2,],
-    array[3,4]
+<?php 
+//$numeros = [1,2,3]; 
+$numeros =[
+        [1,2,3,4,5],
+        [6,7,8,9]
+        
 ];
+//foreach ($colors as $value) {
+    //echo "$value <br>";
+//}
+//$suma=0;
+//for ($i=0; $i< count($numeros); $i++){
+    //$suma= $suma+$numeros[$i];
+///}
+//echo $suma;
 
-echo "resultado".$suma[1][1]
-
-foreach ($suma as $value){
-    echo "$value <br>";
+$suma=0;
+for ($i=0; $i< count($numeros); $i++){
+    for ($w=0; $w< count($numeros[$i]); $w++){
+        $suma= $suma+$numeros[$i][$w];
+    }
+     
 }
+echo $suma;
+
+
+?>
